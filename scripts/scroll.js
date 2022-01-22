9da9b14f7ae5894f0a71ac1020c742fb820fb504
Georgy Krasnov <shimgk1705@gmail.com>
2022-01-22 12:40:12 +0300

    tabs iphone

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,27 +1,34 @@
-const links = document.querySelectorAll(".header-menu__item a")
+const scrollFunc = () => {
+    const links = document.querySelectorAll(".header-menu__item a")
+    const linkCharacteristics = document.querySelector(".card-details__link-characteristics")
 
-seamless.polyfill()
+    seamless.polyfill()
+
+    const newArray = [...links, linkCharacteristics]
 
 
-links.forEach((elem) => {
-    elem.addEventListener('click', (e) => {
-        e.preventDefault()
-        
-        const id = elem.getAttribute('href').substring(1)
-        const section = document.getElementById(id)
-        
-        if (section) {
-            seamless.elementScrollIntoView(section, {
-                behavior: 'smooth',
-                block: 'start'
-            })
-        } else {
-            seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-                behavior: "smooth",
-                block: "center",
-                inline: "center",
-            });
-        }
+    newArray.forEach((elem) => {
+        elem.addEventListener('click', (e) => {
+            e.preventDefault()
+            
+            const id = elem.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+            
+            if (section) {
+                seamless.elementScrollIntoView(section, {
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "center",
+                });
+            }
 
-    })
+        })
 })
+}
+
+scrollFunc()
