8dd84af29ef3937698919b16de9bf4333a65671e
Georgy Krasnov <shimgk1705@gmail.com>
2022-01-24 18:26:21 +0300

    Rendering dynamic data and sending data

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,14 +1,23 @@
 const accordeon = () => {
     const chItems = document.querySelectorAll('.characteristics__item')
+    const items = document.querySelectorAll('.characteristics__item .characteristics__description')
 
-    chItems.forEach(item => {
+    const tabsClose = () => {
+        items.forEach((el) => {
+            el.classList.remove('open');
+            el.style.height = '';
+        });
+    };
+
+    chItems.forEach((item) => {
         const chButton = item.querySelector('.characteristics__title')
         const chContent = item.querySelector('.characteristics__description')
 
-        chButton.addEventListener('click', () => {
+        chButton.addEventListener('click', () => {   
             if (chContent.classList.contains('open')) {
                 chContent.style.height = ''
             } else {
+                tabsClose()
                 chContent.style.height = chContent.scrollHeight + 'px'
             }
 
